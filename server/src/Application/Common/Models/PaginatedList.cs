aa0299137629d5de5b624f2190782c4d5031b4ed
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-11-07 11:31:19 +0200

    Set private constructor for paginated list model, cuz the model should be created only with CreateAsync static method

--- server/src/Application/Common/Models/PaginatedList.cs
+++ server/src/Application/Common/Models/PaginatedList.cs
@@ -4,18 +4,18 @@ using Microsoft.EntityFrameworkCore;
 
 public class PaginatedList<T>
 {
-    public IReadOnlyCollection<T> Items { get; }
-    public int PageNumber { get; }
-    public int TotalPages { get; }
-    public int TotalCount { get; }
-
-    public PaginatedList(IReadOnlyCollection<T> items, int count, int pageNumber, int pageSize)
+    private PaginatedList(IReadOnlyCollection<T> items, int count, int pageNumber, int pageSize)
     {
         PageNumber = pageNumber;
         TotalPages = (int)Math.Ceiling(count / (double)pageSize);
         TotalCount = count;
         Items = items;
     }
+
+    public IReadOnlyCollection<T> Items { get; }
+    public int PageNumber { get; }
+    public int TotalPages { get; }
+    public int TotalCount { get; }
 
     public bool HasPreviousPage => PageNumber > 1;
 
@@ -24,7 +24,11 @@ public class PaginatedList<T>
     public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
     {
         var count = await source.CountAsync();
-        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+        var items = await source
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
 
         return new PaginatedList<T>(items, count, pageNumber, pageSize);
     }
