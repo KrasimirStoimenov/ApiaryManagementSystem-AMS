f85671e3273666c33315c218b1342c2c31d77528
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-11-08 11:20:49 +0200

    Get apiaries query validator fixed to use constants instead magic values

--- server/src/Application/Common/Constants/ApplicationConstants.cs
+++ server/src/Application/Common/Constants/ApplicationConstants.cs
@@ -6,5 +6,9 @@ internal sealed class ApplicationConstants
     {
         public const int DefaultPage = 1;
         public const int DefaultPageSize = 10;
+
+        public const int MinPage = 1;
+        public const int MinPageSize = 1;
+        public const int MaxPageSize = 100;
     }
 }
--- server/src/Application/Features/Apiaries/Queries/GetApiaries/GetApiariesQueryValidator.cs
+++ server/src/Application/Features/Apiaries/Queries/GetApiaries/GetApiariesQueryValidator.cs
@@ -1,16 +1,20 @@
 ﻿namespace ApiaryManagementSystem.Application.Features.Apiaries.Queries.GetApiaries;
 
 using FluentValidation;
+using static ApiaryManagementSystem.Application.Common.Constants.ApplicationConstants.Pagination;
 
 public class GetApiariesQueryValidator : AbstractValidator<GetApiariesQuery>
 {
     public GetApiariesQueryValidator()
     {
         RuleFor(x => x.Page)
-            .GreaterThanOrEqualTo(1).WithMessage("Page should be greater than or equal to 1.");
+            .GreaterThanOrEqualTo(MinPage)
+                .WithMessage($"Page should be greater than or equal to {MinPage}.");
 
         RuleFor(x => x.PageSize)
-            .GreaterThanOrEqualTo(1).WithMessage("Page size should be greater than or equal to 1.")
-            .LessThanOrEqualTo(100).WithMessage("Page size should be less than or equal to 100.");
+            .GreaterThanOrEqualTo(MinPageSize)
+                .WithMessage($"Page size should be greater than or equal to {MinPageSize}.")
+            .LessThanOrEqualTo(MaxPageSize)
+                .WithMessage($"Page size should be less than or equal to {MaxPageSize}.");
     }
 }
