e602272ae6fc0bf7fb9774ff4ca7917c51c2254e
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-12-04 09:12:43 +0200

    Added Create bee queen functionality

---
+++ server/src/Web/Endpoints/BeeQueens.cs
@@ -0,0 +1,23 @@
+﻿namespace ApiaryManagementSystem.Web.Endpoints;
+
+using ApiaryManagementSystem.Application.Features.BeeQueens.Commands.CreateBeeQueen;
+using ApiaryManagementSystem.Web.Infrastructure;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+
+public class BeeQueens : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .RequireAuthorization()
+            .MapPost(CreateBeeQueen);
+    }
+
+    public async Task<IResult> CreateBeeQueen(ISender sender, CreateBeeQueenCommand command)
+    {
+        var beeQueenId = await sender.Send(command);
+
+        return Results.CreatedAtRoute("TBC", new { x = beeQueenId }, beeQueenId);
+    }
+}
