be731ff70ccca896b5066669ea2a82df1519ccea
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-12-09 14:21:21 +0200

    Added delete disease functionality

--- server/src/Web/Endpoints/Diseases.cs
+++ server/src/Web/Endpoints/Diseases.cs
@@ -1,6 +1,7 @@
 ﻿namespace ApiaryManagementSystem.Web.Endpoints;
 
 using ApiaryManagementSystem.Application.Features.Diseases.Commands.CreateDisease;
+using ApiaryManagementSystem.Application.Features.Diseases.Commands.DeleteDisease;
 using ApiaryManagementSystem.Web.Infrastructure;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
@@ -11,7 +12,8 @@ public sealed class Diseases : EndpointGroupBase
     {
         app.MapGroup(this)
             .RequireAuthorization()
-            .MapPost(CreateDisease);
+            .MapPost(CreateDisease)
+            .MapDelete(DeleteDisease, "{id}");
     }
 
     public async Task<IResult> CreateDisease(ISender sender, CreateDiseaseCommand command)
@@ -19,5 +21,12 @@ public sealed class Diseases : EndpointGroupBase
         var diseaseId = await sender.Send(command);
 
         return Results.CreatedAtRoute(nameof(this.CreateDisease), new { id = diseaseId }, diseaseId);
+    }
+
+    public async Task<IResult> DeleteDisease(ISender sender, Guid id)
+    {
+        await sender.Send(new DeleteDiseaseCommand() { DiseaseId = id });
+
+        return Results.NoContent();
     }
 }
