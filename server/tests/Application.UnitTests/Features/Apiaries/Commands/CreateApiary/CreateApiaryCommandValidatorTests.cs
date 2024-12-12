0f5dffa1b05065ee6a9ce7d7229d8d0d0e6586a3
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-12-12 09:48:30 +0200

    Unit tested - Create apiary command validator

---
+++ server/tests/Application.UnitTests/Features/Apiaries/Commands/CreateApiary/CreateApiaryCommandValidatorTests.cs
@@ -0,0 +1,78 @@
+﻿namespace ApiaryManagementSystem.Application.UnitTests.Features.Apiaries.Commands.CreateApiary;
+
+using ApiaryManagementSystem.Application.Features.Apiaries.Commands.CreateApiary;
+using FluentValidation.TestHelper;
+using Xunit;
+
+public class CreateApiaryCommandValidatorTests
+{
+    private readonly CreateApiaryCommandValidator validator;
+
+    public CreateApiaryCommandValidatorTests()
+    {
+        this.validator = new CreateApiaryCommandValidator();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("A")]
+    [InlineData("Test name with maximum length (50 characters or more)")]
+    public async Task Should_HaveValidationError_WhenNameIsNotValid(string? name)
+    {
+        //Arrange
+        var command = new CreateApiaryCommand()
+        {
+            Name = name!,
+            Location = "Test location",
+        };
+
+        //Act
+        var result = await this.validator.TestValidateAsync(command);
+
+        //Assert
+        result.ShouldHaveValidationErrorFor(c => c.Name).Only();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("Test location with maximum length (50 characters or more)")]
+    public async Task Should_HaveValidationError_WhenLocationIsNotValid(string? location)
+    {
+        //Arrange
+        var command = new CreateApiaryCommand()
+        {
+            Name = "Test name",
+            Location = location!,
+        };
+
+        //Act
+        var result = await this.validator.TestValidateAsync(command);
+
+        //Assert
+        result.ShouldHaveValidationErrorFor(c => c.Location).Only();
+    }
+
+    [Theory]
+    [InlineData(null, null)]
+    [InlineData("", "")]
+    [InlineData("Test name with maximum length (50 characters or more)",
+        "Test location with maximum length (50 characters or more)")]
+    public async Task Should_HaveValidationError_WhenNameAndLocationAreNotValid(string? name, string? location)
+    {
+        //Arrange
+        var command = new CreateApiaryCommand()
+        {
+            Name = name!,
+            Location = location!,
+        };
+
+        //Act
+        var result = await this.validator.TestValidateAsync(command);
+
+        //Assert
+        result.ShouldHaveValidationErrorFor(c => c.Name);
+        result.ShouldHaveValidationErrorFor(c => c.Location);
+    }
+}
