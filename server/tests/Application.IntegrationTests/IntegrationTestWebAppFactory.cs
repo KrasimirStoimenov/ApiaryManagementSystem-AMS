85b96cf26b496cb17de0659f0ae006b6d6f5940a
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2024-12-14 14:47:04 +0200

    Added create apiary integration test

---
+++ server/tests/Application.IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,33 @@
+﻿namespace ApiaryManagementSystem.Application.IntegrationTests;
+
+using ApiaryManagementSystem.Application.Common.Interfaces;
+using ApiaryManagementSystem.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("IntegrationTests");
+
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptorType = typeof(DbContextOptions<ApplicationDbContext>);
+            var descriptor = services.SingleOrDefault(s => s.ServiceType == descriptorType);
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services
+                .AddEntityFrameworkInMemoryDatabase()
+                .AddDbContext<IApplicationDbContext, ApplicationDbContext>((sp, options)
+                    => options.UseInMemoryDatabase("TestDb").UseInternalServiceProvider(sp));
+        });
+    }
+}
